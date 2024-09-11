43a5d5c1ce17f10befa26c7de4a7e93f084ec2aa
monster <musakovich@gmail.com>
2024-09-11 22:19:38 +0500

    add check user eligibility

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python
-
 import logging
 
-from config import TOKEN
+from config import TOKEN, eligible_users
 from telegram import Update
 from telegram.ext import Application, MessageHandler, \
     ContextTypes, CommandHandler, filters
@@ -21,26 +19,44 @@ logger = logging.getLogger(__name__)
 
 
 welcome_text = "What do you want to know?"
+not_eligible_user_text = "Sorry, you are not eligible to use this bot"
 
 
-def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-
-    context.bot.send_message(
-        context._chat_id,
-        welcome_text
-        )
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if context._user_id in eligible_users.values():
+        await context.bot.send_message(
+            context._chat_id,
+            welcome_text
+            )
+    else:
+        await context.bot.send_message(
+            context._chat_id,
+            not_eligible_user_text
+            )
 
 
 async def answer(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = update.message.text
-    response = request_to_gpt(text)
-    await update.message.reply_text(response)
+    if context._user_id in eligible_users.values():
+        text = update.message.text
+        response = request_to_gpt(text)
+        await update.message.reply_text(response)
+    else:
+        await context.bot.send_message(
+            context._chat_id,
+            not_eligible_user_text
+            )
 
 
 async def handle_invalid_type(
         update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    await update.message.reply_text("Где-то ошибка. Попробуйте ещё раз.")
-    await context.bot.send_message(context._chat_id, welcome_text)
+    if context._user_id in eligible_users.values():
+        await update.message.reply_text("Где-то ошибка. Попробуйте ещё раз.")
+        await context.bot.send_message(context._chat_id, welcome_text)
+    else:
+        await context.bot.send_message(
+            context._chat_id,
+            not_eligible_user_text
+            )
 
 
 def main() -> None:
